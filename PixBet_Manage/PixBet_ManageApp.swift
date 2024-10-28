2c2b9a0854a84cb9fea9c4df331259ccc6398245
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-10-28 18:42:28 +0300

    Initial Commit

---
+++ PixBet_Manage/PixBet_ManageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixBet_ManageApp.swift
+//  PixBet_Manage
+//
+//  Created by Роман on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PixBet_ManageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
